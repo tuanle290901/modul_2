c58a92d935fc4c4f16b2cc257874ca31a3fdb909
tuanle290901 <tuanle290901@gmail.com>
2023-04-03 15:51:48 +0700

    hello

---
+++ src/BaiTapJame1.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class BaiTapJame1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(" Nhập tên của bạn: ");
+        String name = scanner.nextLine();
+        System.out.println(" Hello "+ name);
+    }
+}
